7f60a3adefdaefdc415ac443e49908c61f19af38
nilkkapaineet <logintili@gmail.com>
2017-12-15 10:58:33 +0200

    Add files via upload
    Day 15, problem a
    Code tided up, main function cleaned up and manipulation placed into methods instead

--- Main.java
+++ Main.java
@@ -16,35 +16,40 @@ public class Main {
         for (long j=0; j<40000000; j++) {
             startingValueA = startingValueA * factorA % divider;
             a = Long.toBinaryString(startingValueA);
-            // if length less than 32, add leading zeros
-            String zeros = "";
-            for (int ali=a.length(); ali<20; ali++) {
-                zeros = zeros.concat("0");
-            }
-            a = zeros.concat(a);
+            a = leadingZeros(a);
             startingValueB = startingValueB * factorB % divider;
 
             b = Long.toBinaryString(startingValueB);
             // if length less than 32, add leading zeros
-            zeros = "";
-            for (int bli=b.length(); bli<20; bli++) {
-                zeros = zeros.concat("0");
-            }
-            b = zeros.concat(b);
+            b = leadingZeros(b);
 
-            boolean match = true;
-            for (int l=0;l<16;l++) {
-                char abit = a.charAt(a.length()-1-l);
-                char bbit = b.charAt(b.length()-1-l);
-                if (abit != bbit) {
-                    match = false;
-                    break;
-                }
-            }
-            if (match) {
+            if (matched(a,b)) {
                 judge++;
             }
         }
         System.out.println("Judge found: " + judge);
+    }
+
+    public static boolean matched(String a, String b) {
+        boolean match = true;
+        for (int l=0;l<16;l++) {
+            char abit = a.charAt(a.length()-1-l);
+            char bbit = b.charAt(b.length()-1-l);
+            if (abit != bbit) {
+                match = false;
+                break;
+            }
+        }
+        return match;
+    }
+
+    public static String leadingZeros(String a) {
+        // if length less than 32, add leading zeros
+        String zeros = "";
+        for (int ali=a.length(); ali<32; ali++) {
+            zeros = zeros.concat("0");
+        }
+        a = zeros.concat(a);
+        return a;
     }
 }
